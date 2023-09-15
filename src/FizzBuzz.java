dd324aece38397d5bb56ba8a6e4a32f0005ba43d
ikraskov <122958228+ikraskov@users.noreply.github.com>
2023-09-15 15:59:00 -0400

    Hop Hey

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -10,7 +10,7 @@ class FizzBuzz {
             i--;
         }
     }
-
+// Hop hey Lalalay
     public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
